ff57238cf39191dfcee92ead00a793b72922ac75
alexgamboa1 <ag.gamboa94@gmail.com>
2020-09-28 23:07:43 -0700

    commit dept schema and date

---
+++ Schema.sql
@@ -0,0 +1,33 @@
+create table departments (
+    department varchar(100),
+    division varchar(100),
+    primary key (department)
+  );
+  
+ insert into departments values ('Clothing','Home');
+insert into departments values ('Grocery','Home');
+insert into departments values ('Decor','Home');
+insert into departments values ('Furniture','Home');
+insert into departments values ('Computers','Electronics');
+insert into departments values ('Device Repair','Electronics');
+insert into departments values ('Phones & Tablets','Electronics');
+insert into departments values ('Garden','Outdoors');
+insert into departments values ('Camping & Fishing','Outdoors');
+insert into departments values ('Sports','Outdoors');
+insert into departments values ('Children Clothing','Kids');
+insert into departments values ('Toys','Kids');
+insert into departments values ('Vitamins','Health');
+insert into departments values ('Pharmacy','Health');
+insert into departments values ('First Aid','Health');
+insert into departments values ('Automotive','Hardware');
+insert into departments values ('Tools','Hardware');
+insert into departments values ('Jewelry','Fashion');
+insert into departments values ('Beauty','Fashion');
+insert into departments values ('Cosmetics','Fashion');
+insert into departments values ('Books','Entertainment');
+insert into departments values ('Games','Entertainment');
+insert into departments values ('Music','Entertainment');
+insert into departments values ('Movies','Entertainment');
+
+Select * FROM departments 
+
